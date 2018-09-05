b214247712f12fad562d2f8d1ecbd52c059ef848
mumudong <850886448@qq.com>
2018-09-05 16:32:04 +0800

    asd

--- spark2/src/main/java/mumudong/core/Core.scala
+++ spark2/src/main/java/mumudong/core/Core.scala
@@ -3,27 +3,38 @@ package mumudong.core
 import org.junit.Test
 
 /**
-  *        ┏┓　　　┏┓
-  *　　　┏┛┻━━━┛┻┓
-  *　　　┃　　　　　　　┃ 　
-  *　　　┃　　　━　　　┃
-  *　　 ████━████ ┃
-  *　　　┃　　　　　　　┃
-  *　　　┃　　　┻　　　┃
-  *　　　┃　　　　　　　┃
-  *　　　┗━┓　　　┏━┛
-  *　　　　　┃　　　┃　　　　　　　　　　　
-  *　　　　　┃　　　┃
-  *　　　　　┃　　　┃　　　　Codes are far away from bugs with the animal protecting　　　
-  *　　　　　┃　　　┃   　　　　          神兽保佑,代码无bug　　
-  *　　　　　┃　　　┃
-  *　　　　　┃　　　┃　　 　　　　　　　　　
-  *　　　　　┃　 　　┗━━━┓
-  *　　　　　┃ 　　　　　　　┣┓
-  *　　　　　┃ 　　　　　　　┏┛
-  *　　　　　┗┓┓┏━┳┓┏┛
-  *　　　　　　┃┫┫　┃┫┫
-  *　　　　　　┗┻┛　┗┻┛
+  * ----> count
+  * val c = sc.parallelize(List("Gnu", "Cat", "Rat", "Dog"), 2)
+    c.count
+    res2: Long = 4
+  *
+  * ----> countByKey
+  * val c = sc.parallelize(List((3, "Gnu"), (3, "Yak"), (5, "Mouse"), (3, "Dog")), 2)
+  * c.countByKey
+  * res1: scala.collection.Map[Int,Long] = Map(3 -> 3, 5 -> 1)
+  * c.countByKey.size
+  * res2: Int = 2
+  *
+  * ----> countByValue
+  * scala> val b = sc.parallelize(List(1,2,3,4,5,6,7,8,2,4,2,1,1,1,1,1))
+  * b: org.apache.spark.rdd.RDD[Int] = ParallelCollectionRDD[12] at parallelize at <console>:21
+  *
+  * scala> b.countByValue
+  * res3: scala.collection.Map[Int,Long] = Map(5 -> 1, 1 -> 6, 6 -> 1, 2 -> 3, 7 -> 1, 3 -> 1, 8 -> 1, 4 -> 2)
+  *
+  * ----> groupByKey
+  *  val scoreDetail = sc.parallelize(List(("xiaoming",75),("xiaoming",90),("lihua",95),("lihua",100),("xiaofeng",85)))
+  *  scoreDetail.groupByKey().collect().foreach(println(_));
+  *    (lihua,CompactBuffer(95, 100))
+  *    (xiaoming,CompactBuffer(75, 90))
+  *    (xiaofeng,CompactBuffer(85))
+  *
+  * ----> cogroup
+  * scala> val scoreDetail = sc.parallelize(List(("xiaoming",95),("xiaoming",90),("lihua",95),("lihua",98),("xiaofeng",97)))
+  * scala> val scoreDetai2 = sc.parallelize(List(("xiaoming",65),("lihua",63),("lihua",62),("xiaofeng",67)))
+  * scala> val scoreDetai3 = sc.parallelize(List(("xiaoming",25),("xiaoming",15),("lihua",35),("lihua",28),("xiaofeng",36)))
+  * scala> scoreDetail.cogroup(scoreDetai2,scoreDetai3)
+  * res1: Array[(String, (Iterable[Int], Iterable[Int], Iterable[Int]))] = Array((xiaoming,(CompactBuffer(95, 90),CompactBuffer(65),CompactBuffer(25, 15))), (lihua,(CompactBuffer(95, 98),CompactBuffer(63, 62),CompactBuffer(35, 28))), (xiaofeng,(CompactBuffer(97),CompactBuffer(67),CompactBuffer(36))))
   */
 class Core {
     @Test
--- spark2/src/main/java/mumudong/core/StateTestTwo.scala
+++ spark2/src/main/java/mumudong/core/StateTestTwo.scala
@@ -1,5 +1,9 @@
 package mumudong.core
 
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
 /**
   *        ┏┓　　　┏┓+ +
   *　　　┏┛┻━━━┛┻┓ + +
@@ -25,6 +29,21 @@ package mumudong.core
   */
 object StateTestTwo {
     def main(args: Array[String]): Unit = {
+        System.setProperty("HADOOP_USER_NAME","hdfs")
+        val sparkConf = new SparkConf().setMaster("local[2]")
+                                        .setAppName(getClass.getSimpleName)
+        val ssc = new StreamingContext(sparkConf,Seconds(5))
+        ssc.checkpoint("/test/mapstate")
+        val lines = ssc.socketTextStream("hadoop-7",19999)
+        lines.flatMap(_.split(" "))
+                 .map(word => (word,1))
+                .updateStateByKey { (curValues: Seq[Int], preValue: Option[Int]) =>
+                    val curValueSum = curValues.sum
+                    Some(curValueSum + preValue.getOrElse(0))
+                }.print() //会保留所有数据的累计值
+        ssc.start()
+        ssc.awaitTermination()
+        ssc.stop()
 
     }
 }
