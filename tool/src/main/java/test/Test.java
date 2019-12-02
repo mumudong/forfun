49957147bf10b87345732f34650f58d08b7ea912
mushudong <850886448@qq.com>
2019-12-02 15:53:21 +0800

    123

--- tool/src/main/java/test/Test.java
+++ tool/src/main/java/test/Test.java
@@ -4,11 +4,10 @@ import java.util.HashMap;
 
 public class Test {
     public static void main(String[] args) {
-        HashMap<String,String> map = new HashMap<>();
-        map.put("a","1");
-        map.put("b","2");
-//        map.compute("a",(k,v) -> "11");
-        map.computeIfAbsent("c",k -> null);
-        System.out.println(map);
+        Test test = new Test2();
+        System.out.println(test.getClass());
     }
+}
+class Test2 extends Test{
+
 }
