00f627dbec7d7d3b76554518026a577cb496a213
mumudong <850886448@qq.com>
2020-05-08 23:44:33 +0800

    123

---
+++ flink/src/main/java/datastream/MetricsTest.scala
@@ -0,0 +1,14 @@
+package datastream
+
+/**
+  * MetricGroup是用来对Metric进行分组管理，MetricReporter是用来对外披露Metric，
+  * 而MetricRegistry就是这两者之间的桥梁，通过MetricRegistry，就可以让MetricReporter感知到在MetricGroup中的Metric发生的变化情况。
+  *
+  */
+object MetricsTest {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
+
+class
